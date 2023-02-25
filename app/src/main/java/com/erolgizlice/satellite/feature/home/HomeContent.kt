1d8b6745375e758410148b12efc169b868d80ad9
erolgizlice <erolgizlice@gmail.com>
2023-02-25 14:20:31 +0300

    added search bar

--- app/src/main/java/com/erolgizlice/satellite/feature/home/HomeContent.kt
+++ app/src/main/java/com/erolgizlice/satellite/feature/home/HomeContent.kt
@@ -1,23 +1,59 @@
 package com.erolgizlice.satellite.feature.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Divider
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.erolgizlice.satellite.core.model.data.Satellite
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeContent(
     satelliteList: List<Satellite>,
     onSatelliteClick: (Int) -> Unit,
-    modifier: Modifier
+    modifier: Modifier,
+    searchState: SearchState = rememberSearchState()
+) {
+    LaunchedEffect(searchState.query.text) {
+        searchState.searchResults =
+            if (searchState.query.text.length > 2)
+                satelliteList.filter {
+                    it.name.contains(searchState.query.text, ignoreCase = true)
+                }
+            else satelliteList
+    }
+
+    Column(modifier = modifier) {
+        SearchBar(
+            modifier = Modifier
+                .padding(16.dp),
+            query = searchState.query,
+            onQueryChange = { searchState.query = it },
+        )
+        ShowSatellites(
+            modifier = modifier,
+            satelliteList = searchState.searchResults,
+            onSatelliteClick = onSatelliteClick
+        )
+    }
+}
+
+@Composable
+fun ShowSatellites(
+    modifier: Modifier,
+    satelliteList: List<Satellite>,
+    onSatelliteClick: (Int) -> Unit
 ) {
     LazyColumn(
         modifier = modifier
@@ -43,4 +79,77 @@ fun HomeContent(
             }
         }
     }
+}
+
+@Composable
+private fun SearchBar(
+    query: TextFieldValue,
+    onQueryChange: (TextFieldValue) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = Color.LightGray,
+        shape = RoundedCornerShape(38.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    ) {
+        Box(Modifier.fillMaxSize()) {
+            if (query.text.isEmpty()) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                ) {
+                    Spacer(Modifier.width(48.dp))
+                    Text(
+                        text = "Search",
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentHeight()
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Outlined.Search,
+                        contentDescription = "search"
+                    )
+                }
+                BasicTextField(
+                    value = query,
+                    onValueChange = onQueryChange,
+                    modifier = Modifier
+                        .weight(1f),
+                    textStyle = MaterialTheme.typography.body1
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun rememberSearchState(
+    query: TextFieldValue = TextFieldValue(""),
+    searchResults: List<Satellite> = emptyList()
+): SearchState {
+    return remember {
+        SearchState(
+            query = query,
+            searchResults = searchResults
+        )
+    }
+}
+
+@Stable
+class SearchState(
+    query: TextFieldValue,
+    searchResults: List<Satellite>
+) {
+    var query by mutableStateOf(query)
+    var searchResults by mutableStateOf(searchResults)
 }
